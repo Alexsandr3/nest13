249143f958bcac6d012a95eaa17f8142a9e099dd
Alexsandr3 <fam_86@mail.ru>
2022-12-03 17:33:47 +0100

    home task 13 Nest js vercel.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { BlogModule } from './blogs/blog.module';
-import { PostModule } from './posts/post.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { BlogModule } from "./blogs/blog.module";
+import { PostModule } from "./posts/post.module";
 import { CommentModule } from "./comments/comment.module";
 import { UserModule } from "./users/user.module";
-import { MailModule } from './mail/mail.module';
+import { MailModule } from "./mail/mail.module";
 import { AuthModule } from "./auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
 import { TestingModule } from "./testing/testing.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      `mongodb+srv://admin:admin@nestjs.vw3gy4j.mongodb.net/?retryWrites=true&w=majority`,
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     BlogModule,
     PostModule,
     CommentModule,
     UserModule,
     MailModule,
     AuthModule,
-    ConfigModule.forRoot({
-      isGlobal: true, // no need to import into other modules
-    }),
+    ConfigModule.forRoot({ isGlobal: true}), // no need to import into other modules,
     TestingModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
