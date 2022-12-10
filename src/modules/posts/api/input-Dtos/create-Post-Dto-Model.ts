7b9661fa3c59fb66161611728bbd83e6a6015bcf
Alexsandr3 <fam_86@mail.ru>
2022-12-10 11:26:21 +0100

    home task 14 Nest js add guard for get

--- src/modules/posts/api/input-Dtos/create-Post-Dto-Model.ts
+++ src/modules/posts/api/input-Dtos/create-Post-Dto-Model.ts
@@ -1,4 +1,4 @@
-import { IsString, Length } from 'class-validator';
+import { IsMongoId, IsString, Length } from "class-validator";
 import { Transform } from 'class-transformer';
 
 export class CreatePostDto {
@@ -28,5 +28,8 @@ export class CreatePostDto {
    */
   @Transform(({ value }) => value.trim())
   @IsString()
+  @IsMongoId()
   blogId: string;
 }
+
+
