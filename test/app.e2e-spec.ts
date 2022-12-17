df7cb4760b6efefe4061d0b5bf9820421c66bff0
Alexsandr3 <fam_86@mail.ru>
2022-12-17 09:43:10 +0100

    home task 16 Nestjs add tests for auth 09

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -261,7 +261,7 @@ describe("AppController (e2e)", () => {
         .expect(401);
     });
   });
-  describe(`sa`, ()=>{
+  describe.skip(`sa`, ()=>{
     beforeAll(async () => {
       await request(app.getHttpServer())
         .delete(`/testing/all-data`).expect(204);
